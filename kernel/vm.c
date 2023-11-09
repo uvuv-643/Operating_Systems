d897429568ae2fbe67c942a001efefd29ae6482c
Artem <uvuv.space@gmail.com>
2023-11-10 00:21:22 +0300

    little cleanup

--- kernel/vm.c
+++ kernel/vm.c
@@ -307,35 +307,6 @@ uvmfree(pagetable_t pagetable, uint64 sz)
 // physical memory.
 // returns 0 on success, -1 on failure.
 // frees any allocated pages on failure.
-// int
-// uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
-// {
-//   pte_t *pte;
-//   uint64 pa, i;
-//   uint flags;
-//   char *mem;
-
-//   for(i = 0; i < sz; i += PGSIZE){
-//     if((pte = walk(old, i, 0)) == 0)
-//       panic("uvmcopy: pte should exist");
-//     if((*pte & PTE_V) == 0)
-//       panic("uvmcopy: page not present");
-//     pa = PTE2PA(*pte);
-//     flags = PTE_FLAGS(*pte);
-//     if((mem = kalloc()) == 0)
-//       goto err;
-//     memmove(mem, (char*)pa, PGSIZE);
-//     if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-//       kfree(mem);
-//       goto err;
-//     }
-//   }
-//   return 0;
-
-//  err:
-//   uvmunmap(new, 0, i / PGSIZE, 1);
-//   return -1;
-// }
 int
 uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 {
@@ -506,7 +477,7 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   }
 }
 
-
+// When started writing to page, copying memory
 int cow_copy(pagetable_t pagetable, uint64 va) {
   pte_t* pte = walk(pagetable, va, 0);
   uint flags = PTE_FLAGS(*pte);
