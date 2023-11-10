fa5662a178474d09ae4953d744e0e13474156fb7
Artem <uvuv.space@gmail.com>
2023-11-10 18:27:00 +0300

    fixes to proc pool

--- kernel/proc/proc_pool.c
+++ kernel/proc/proc_pool.c
@@ -8,6 +8,7 @@
 
 struct memory_pool {
     struct proc procs[NPROC];
+    struct memory_pool* prev;
     struct memory_pool* next;
 };
 
@@ -17,6 +18,7 @@ int proc_index = 0;
 // how often after free system has to find unused pools
 int proc_interval = NPROC / 2;
 int proc_interval_current = 0;
+int min_active_proc_count_in_pool = NPROC / 2;
 
 struct memory_pool* head;
 struct spinlock pool_lock;
@@ -27,9 +29,9 @@ struct spinlock interval_lock;
 // function must be called once when the proccess module starts
 // no lock has to be acquired
 void init_memory_pool() {
-    head = (struct memory_pool* )malloc(sizeof(struct memory_pool));
     initlock(&pool_lock, "pool_lock");
     initlock(&interval_lock, "interval_lock");
+    head = (struct memory_pool* )malloc(sizeof(struct memory_pool));
     memset(head, 0, sizeof(struct memory_pool));
 }
 
@@ -38,30 +40,72 @@ void init_memory_pool() {
 // pool is unused when all the processes have state "UNUSED"
 // no lock has to be acquired before entering function
 void free_unused_memory_pools() {
+
     acquire(&pool_lock);
-    struct memory_pool* current_pool = head;
-    while (current_pool != 0 && current_pool->next != 0) {
-        struct memory_pool* next_pool = current_pool->next;
+
+    // that is pools which will be merged (?) by this function
+    struct memory_pool* first_small_pool = 0;
+    int first_small_pool_cnt = 0;
+    struct memory_pool* second_small_pool = 0;
+    int second_small_pool_cnt = 0;
+
+    struct memory_pool* current_pool = head->next;
+
+    // there we find two smallest pools for merging
+    // if merge is possible
+    // first_small_pool != 0 && second_small_pool != 0
+    while (current_pool != 0) {
         int number_of_used_procs = 0;
         for (int i = 0; i < NPROC; i++) {
-            if (next_pool->procs->state != UNUSED) {
+            if (current_pool->procs->state != UNUSED) {
                 number_of_used_procs++;
                 break;
             }
         }
-        if (!number_of_used_procs) {
-            current_pool->next = 0;
-            struct memory_pool* have_to_be_removed = next_pool;
-            struct memory_pool* mpp = have_to_be_removed;
-            while (mpp != 0) {
-                mpp = mpp->next;
-                free(have_to_be_removed);
-                have_to_be_removed = mpp;
+        if (number_of_used_procs < min_active_proc_count_in_pool) {
+            if (first_small_pool && !second_small_pool) {
+                second_small_pool = current_pool;
+                second_small_pool_cnt = number_of_used_procs;
             }
-            return;  
+            else if (!first_small_pool) {
+                first_small_pool = current_pool;
+                first_small_pool_cnt = number_of_used_procs;
+            } else {
+                if (first_small_pool_cnt > second_small_pool_cnt) {
+                    if (number_of_used_procs < first_small_pool_cnt) {
+                        first_small_pool = current_pool;
+                        first_small_pool_cnt = number_of_used_procs;
+                    }
+                } else {
+                    if (number_of_used_procs < second_small_pool_cnt) {
+                        second_small_pool = current_pool;
+                        second_small_pool_cnt = number_of_used_procs;
+                    }
+                }
+            }
         }
         current_pool = current_pool->next;
     }
+
+    // merge two pools
+    if (first_small_pool != 0 && second_small_pool != 0) {
+        struct memory_pool* pool = (struct memory_pool*) malloc(sizeof(struct memory_pool));
+        memset(head, 0, sizeof(struct memory_pool));
+        int l = 0; int r = 0;
+        while (l < first_small_pool_cnt && r < second_small_pool_cnt) {
+            if (l < first_small_pool_cnt && r < second_small_pool_cnt) {
+                if (first_small_pool->procs[l].pid < second_small_pool->procs[r].pid) {
+                    pool->procs[l + r] = first_small_pool->procs[l];
+                    l++;
+                } else {
+                    pool->procs[l + r] = second_small_pool->procs[r];
+                    r++;
+                }
+            }
+        }
+    }
+    
+    proc_interval_current = 0;
     release(&pool_lock);
 }
 
@@ -78,8 +122,10 @@ struct proc* take_from_pool() {
     return 0;
 }
 
-void free_from_pool() {
-
+void free_from_pool(struct proc* proc) {
+    acquire(&proc->lock);
+    proc->state = UNUSED;
+    release(&proc->lock);
     acquire(&interval_lock);
     proc_interval_current++;
     if (proc_interval_current >= proc_interval) {
